38dcbd9cf8a68095057c650ce0110582e16de08a
jahir <rana78085@gmail.com>
2022-10-27 19:35:19 +0600

    Add homepage content

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,9 +1,53 @@
 import React from 'react';
+import './home.css';
 
 const Home = () => {
     return (
-        <div>
-            <h2>This is Home page.</h2>
+        <div className='home'>
+            <h2 className='text-center my-5 text-capitalize'>Welcome to web development course</h2>
+            <div className='container'>
+                <div className=' grid'>
+                    <div className=''>
+                        <img src='https://www.simplilearn.com/ice9/free_resources_article_thumb/Best-Programming-Languages-to-Start-Learning-Today.jpg' alt=''></img>
+                    </div>
+                    <div className=''>
+                        <img src='https://insights.dice.com/wp-content/uploads/2017/07/Programming-Language-Popularity.jpg' alt=''></img>
+                    </div>
+                    <div className=''>
+                        <img src='https://www.makewonder.com/wp-content/uploads/sites/4/2021/01/coding-languages-image.jpeg' alt=''></img>
+                    </div>
+                    <div>
+                        <img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSr0Z53hAJ45o4dPjh1BUHjo0IJQsVtJVravCgFvSy_VB544n6Sp-Z7o3DKKybuM8KFIlU&usqp=CAU' alt=''></img>
+                    </div>
+                    <div>
+                        <img src='https://woz-u.com/wp-content/uploads/2020/04/software-programming-online-1-woz-ucom.jpg' alt=''></img>
+                    </div>
+                    <div>
+                        <img src='https://media-exp1.licdn.com/dms/image/C4D08AQEm55Tz9Ibr_Q/croft-frontend-shrinkToFit1920/0/1597380770151?e=2159024400&v=beta&t=myB06fIpq1TR0d25wMO1_3YDGWalffor66rShlXu1vs' alt=''></img>
+                    </div>
+                    <div>
+                        <img src='https://www.lighthouselabs.ca/uploads/post/open_graph_image/380/Why_You_Should_Learn_Python.jpg' alt=''></img>
+                    </div>
+                    <div>
+                        <img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSoydI8C0XclNV5V4zdfEJsu9fUSAZhPnaRqGzKy3nJ_wIbm0KOaTnhrDIStYqyR2bAC_Q&usqp=CAU' alt=''></img>
+                    </div>
+                    <div>
+                        <img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTdXVB_8bBqI68zIXw_yI80dGHcth2K0k5WsE1Rn7fIgk_Mhpl2k0nJQoS594ItCsJchiM&usqp=CAU' alt=''></img>
+                    </div>
+                    <div>
+                        <img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS_N6qUSR1H-67MseTjh05vaeAaZHmcq3SfT5yXaCoRIvwYMiBiQdybVvolygr-n0-ejaY&usqp=CAU' alt=''></img>
+                    </div>
+                    <div>
+                        <img src='https://www.wikihow.com/images/thumb/a/ae/Start-Learning-Computer-Programming-Step-1-Version-4.jpg/v4-460px-Start-Learning-Computer-Programming-Step-1-Version-4.jpg.webp' alt=''></img>
+                    </div>
+                    <div>
+                        <img src='https://www.wikihow.com/images/thumb/3/3c/Start-Learning-Computer-Programming-Step-16-Version-2.jpg/v4-460px-Start-Learning-Computer-Programming-Step-16-Version-2.jpg' alt=''></img>
+                    </div>
+                </div>
+            </div>
+            <footer className='my-5'>
+                <p className='text-center'>&copy; Copyright 2022</p>
+            </footer>
         </div>
     );
 };
