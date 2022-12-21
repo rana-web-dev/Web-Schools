f090ed6ba6983075c9b32c2c4166f18112936e7d
Rana Web Developer <73062848+rana-web-dev@users.noreply.github.com>
2022-12-21 18:36:41 +0600

    Update Html.js
    Fixed Unhanlde Throw Error

--- src/components/coursePage/Html.js
+++ src/components/coursePage/Html.js
@@ -13,7 +13,7 @@ const Html = () => {
         <div className='d-flex mt-3'>
             <div className='aside border-end px-3' style={{height: 1000}}>
                 {
-                    html.map(html => 
+                    html?.map(html => 
                     <li className='list-unstyled py-1'>
                         <Link className='text-decoration-none' key={html.id} to={`${html.id}`}>{html.name}</Link>
                     </li>)
